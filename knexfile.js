2d417f143eb4b3a8624153f1a8cbb004d45db306
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-07-14 09:39:56 -0600

    add connection details

--- knexfile.js
+++ knexfile.js
@@ -2,41 +2,49 @@
 const connectionString = process.env.DATABASE_URL;
 
 module.exports = {
-
   development: {
-    client: 'pg',
-    connection: connectionString,
+    client: "pg",
+    connection: {
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'pg',
-    connection: connectionString,
+    client: "pg",
+    connection: {
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
