03eee760b9dbe304a4941c5f9b092cc9457a2254
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-07-14 09:44:29 -0600

    fix migrations

--- seeds/initial_cats.js
+++ seeds/initial_cats.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cats').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('cats').insert([
         {id: 1, name:'Hanzo', breed:'American shorthair', coat_color:'black', favorite_treat:'buffalo wings'},
         {id: 2, name:'Midori', breed:'American shorthair', coat_color:'tabby', favorite_treat:'squid jerky'},
         {id: 3, name:'Jesse', breed:'Hemingway', coat_color:'black/white', favorite_treat:'salmon'},
